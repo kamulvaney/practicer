88cb365d874cda8658251339201c9f51a3a72fc8
Kathleen Mulvaney <kmulvane@epic.com>
2020-02-11 20:06:44 -0800

    solve problems for final

--- practice.R
+++ practice.R
@@ -1,7 +1,8 @@
 ## Practice the following problem 
 
 # What is the sum of the first 1000 positive integers?
-sum(1:1000)
+x=1:1000
+sum(x)
 #1. Use the function c to create a vector with the average high temperatures in January for
 #Beijing, Lagos, Paris, Rio de Janeiro, San Juan, and Toronto, which are 35, 88, 42, 84, 81, 
 #and 30 degrees Fahrenheit. Call the object temp.
@@ -10,10 +11,15 @@ temp=c(35,88,42,84,81,30)
 city=c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
 # Use the names function and the objects defined in the previous exercises to associate the temperature data with its corresponding city.
 names(temp)=city
+#use dataframe 
+city_temp=data.frame(city,temp)
 # Use the [ and : operators to access the temperature of the first three cities on the list.
-temp[1:3]           
+temp[1:3]
+city_temp$temp[1:3]
 # Use the [ operator to access the temperature of Paris and San Juan.
-temp[c("Paris", "San Juan")]                        
+temp[c("Paris", "San Juan")] 
+  #dataframe
+city_temp[c(3,5),]
 # Use the : operator to create a sequence of numbers  12,13,14,.....73
 seqa=c(12:73)                       
 # Create a vector containing all the positive odd numbers smaller than 100.
@@ -42,3 +48,4 @@ toCel=function(ftemp){
 }
 toCel(temp)
 
+city_temp$temp=5/9*(city_temp$temp-32)
